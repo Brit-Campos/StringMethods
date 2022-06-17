60c5e770c9e62ff6b5e50317aedcdb9a70468a68
Brit <Brit@DESKTOP-BFORP22>
2022-06-16 17:50:18 -0700

    Add project files.

---
+++ StringMethods/Program.cs
@@ -0,0 +1,21 @@
+﻿String fullName = "Brit Campos";
+String phoneNumber = "123-456-7890";
+
+//fullName = fullName.ToUpper();
+//fullName = fullName.ToLower();
+//Console.WriteLine(fullName);
+
+//phoneNumber = phoneNumber.Replace("-", "/");
+//Console.WriteLine(phoneNumber);
+
+//String userName = fullName.Insert(0, "Mr. ");
+//Console.WriteLine(userName);
+
+//Console.WriteLine(fullName.Length);
+
+String firstName = fullName.Substring(0, 4);
+String lastName = fullName.Substring(5, 6);
+Console.WriteLine(firstName);
+Console.WriteLine(lastName);
+
+Console.ReadKey();
